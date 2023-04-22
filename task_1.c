bd1cc473f30c2b766be0c6b063016c7e91c64052
Sumshi <sumayasomow@gmail.com>
2023-04-22 17:25:27 +0000

    fix

--- task_1.c
+++ task_1.c
@@ -7,28 +7,19 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;/*stores count*/
 	int j, i, d;/*for iterating*/
 	va_list args;
 
 	va_start(args, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')/*before specifier percent*/
-		{
-			format++;
 			switch (*format)
 			{
-
 				case 'i':
 					i = va_arg(args, int);
 					putchar(i);
-					count++;
 					break;
 				case 'd':
 					d = va_arg(args, int);
 					putchar(d);
-					count++;
 					break;
 
 				default:
