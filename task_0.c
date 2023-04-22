6e326d10b4099904d8679a5699ae291d482bc0f5
Sumshi <sumayasomow@gmail.com>
2023-04-22 12:37:19 +0000

    task 0

---
+++ task_0.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _printf - function
+ * @format: contains types of arguments
+ * Return: Always 0
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;/*stores count*/
+	int i;/*for iterating*/
+	char *s;
+	char c;
+	va_list args;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')/*before specifier percent*/
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':/*as in character*/
+					c = va_arg(args, int);
+					putchar(c);
+					count++;
+					break;
+				case 's':/*string*/
+					s = va_arg(args, char *);
+					for (i = 0; s[i] != '\0'; i++)
+					{
+						putchar(*s);
+						count++;
+					}
+					break;
+				case '%':/*percent symbol*/
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar(*format);
+				break;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
