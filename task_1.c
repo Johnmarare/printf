3fa85b406b1f822a9311b4902020b9910516edbb
Johnmarare <mararejohn@gmail.com>
2023-04-22 14:12:50 +0000

    handle %d && %i

---
+++ task_1.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _printf - function
+ * @format: contains types of arguments
+ * Return: Always 0
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0;/*stores count*/
+	int j, i, d;/*for iterating*/
+	va_list args;
+
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')/*before specifier percent*/
+		{
+			format++;
+			switch (*format)
+			{
+
+				case 'i':
+					i = va_arg(args, int);
+					putchar(i);
+					count++;
+					break;
+				case 'd':
+					d = va_arg(args, int);
+					putchar(d);
+					count++;
+					break;
+
+				default:
+
+					break;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
