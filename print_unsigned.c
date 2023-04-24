dcc20207c79d38259a1c7ab2c90649181d30df33
Sumshi <sumayasomow@gmail.com>
2023-04-24 11:52:00 +0000

    tests

--- print_unsigned.c
+++ print_unsigned.c
@@ -11,8 +11,32 @@
 /*UINT is maximum value of unsigned int, u can use 4294967295 but with bool*/
 int print_unsigned(unsigned int num)
 {
-	/*Check if num is within the range of valid unsigned int values*/
+	unsigned int sum;
+
+	/* Check if num is negative */
+	if ((int)num < 0)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	/* Check if num is within the range of valid unsigned int values */
 	if (num > UINT_MAX)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	/* Check for arithmetic overflow */
+	sum = num + 1;
+	if (sum < num)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	/* Check for undefined behavior in bitwise shifts */
+	if (num >= sizeof(unsigned int) * CHAR_BIT)
 	{
 		printf("Error\n");
 		return (-1);
