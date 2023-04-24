d3067102f6d9e766ec0c955e5409e7cd5a8c0ace
Sumshi <sumayasomow@gmail.com>
2023-04-24 19:26:40 +0000

    printf

--- printbinary.c
+++ printbinary.c
@@ -13,15 +13,8 @@ int _printbinary(unsigned int value)
 
 	if (value <= 0)
 	{
-		return (-1);
-	}
-	if (value == 0)
-	{
-		_putchar('0');
 		return (1);
 	}
-
-
 	mod = value % 2;
 	quot = value / 2;
 
