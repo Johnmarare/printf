b40a6b299b94b487c442af3ec69ec3c80fcf83fe
Sumshi <sumayasomow@gmail.com>
2023-04-24 14:29:35 +0000

    fix bugs

--- print_HEX.c
+++ print_HEX.c
@@ -2,12 +2,35 @@
 #include "main.h"
 #include<limits.h>
 /**
- * print_hex- prints hexadecimal  of a number in lowercase
+ * print_HEX - prints hexadecimal  of a number in uppercase
  * @n: parameter
  * Return: Always 0
  */
 int print_HEX(unsigned int n)
 {
-	printf("%X\n", n);
+	char hex[100];
+	int i = 0;
+	int remainder;
+	int j;
+
+	while (n != 0)
+	{
+		remainder = n % 16;
+		if (remainder < 10)
+			hex[i] = remainder + '0';
+		else
+			hex[i] = remainder - 10 + 'A';/*Use for uppercase*/
+		n /= 16;
+		i++;
+	}
+
+	if (i == 0)
+		_putchar('0');
+	else
+	{
+		for (j = i - 1; j >= 0; j--)
+			_putchar(hex[j]);
+	}
+
 	return (0);
 }
--- print_hex.c
+++ print_hex.c
@@ -1,6 +1,4 @@
-#include<stdio.h>
-#include "main.h"
-#include<limits.h>
+#include"main.h"
 /**
  * print_hex- prints hexadecimal  of a number in lowercase
  * @n: parameter
@@ -8,6 +6,31 @@
  */
 int print_hex(unsigned int n)
 {
-        printf("%x\n", n);
-        return (0);
+	char hex[100];
+	int i = 0;
+	int j;
+	int remainder;
+
+	while (n != 0)
+	{
+		remainder = n % 16;
+		if (remainder < 10)
+			hex[i] = remainder + '0';
+		else
+			hex[i] = remainder - 10 + 'a';
+		n /= 16;
+		i++;
+	}
+
+	if (i == 0)
+		_putchar('0');
+	else
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			_putchar(hex[j]);
+		}
+	}
+
+	return (0);
 }
