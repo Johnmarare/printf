1028c452621374447cfb528f1b82d4b0068822c2
Sumshi <sumayasomow@gmail.com>
2023-04-25 21:47:57 +0000

    fix

--- print_address.c
+++ print_address.c
@@ -6,8 +6,12 @@
  */
 int _print_address(void *p)
 {
+
 	unsigned long int address = (unsigned long int) p;
 	int count = 0;
+
+	if (p == NULL)
+		p = "(null)";
 
 	count += _putchar('0');
 	count += _putchar('x');
