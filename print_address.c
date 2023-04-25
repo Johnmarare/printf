6d7e3428be0aecaf108f1c49113a53cf51c0db7d
Sumshi <sumayasomow@gmail.com>
2023-04-25 18:59:14 +0000

    tests

--- print_address.c
+++ print_address.c
@@ -1,28 +1,17 @@
 #include "main.h"
 /**
- *_print_address - printf address
+ *_print_address - prints address
  * @p: parameter
  * Return: number of characters printed
  */
 int _print_address(void *p)
 {
-	char *s = "(nil)";
-	long int a;
-	int b;
-	int i;
+	unsigned long int address = (unsigned long int) p;
+	int count = 0;
 
-	if (p == NULL)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
-		return (i);
-	}
+	count += _putchar('0');
+	count += _putchar('x');
+	count += print_hex_lower(address);
 
-	a = (unsigned long int)p;
-	_putchar('0');
-	_putchar('x');
-	b = print_hex_lower(a);
-	return (b + 2);
+	return (count);
 }
